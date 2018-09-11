b162a55d1232c18b0a65bf2fdd9f8a25f829d3e5
Daniel Lenski <dlenski@gmail.com>
2018-09-11 14:44:10 -0700

    initial commit

---
+++ client.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+import random
+import requests
+import argparse
+from requests import Request
+from xml.etree import ElementTree as ET
+from Crypto.PublicKey import RSA
+from Crypto.Util import number
+from Crypto.Cipher import PKCS1_OAEP
+
+from common import e64s, d64s, d64b, d64sb, hexlify
+
+########################################
+
+class Soapifier(object):
+    soap_env_tmpl = '''<?xml version="1.0" encoding="UTF-8"?>
+      <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <soapenv:Body>
+          <{0} xmlns="http://ctkipservice.rsasecurity.com">
+            <AuthData >{2}</AuthData>
+            <ProvisioningData>{3}</ProvisioningData>
+            <{1}>{4}</{1}>
+          </{0}>
+        </soapenv:Body>
+      </soapenv:Envelope>'''
+
+    def __init__(self, url, auth):
+        self.url = url
+        self.auth = auth
+
+    def make_ClientRequest(self, action, provisioning_data, body):
+        outer, inner = 'ClientRequest', 'Request'
+        soap = self.soap_env_tmpl.format(
+            outer, inner, self.auth,
+            e64s(provisioning_data), e64s(body))
+        return Request('POST', self.url, data=soap, headers={
+            'Authorization': self.auth,
+            'SOAPAction': action,
+            'content-type': 'application/vnd.otps.ctk-kip'})
+
+    def parse_ServerResponse(self, response):
+        outer, inner = 'ServerResponse', 'Response'
+
+        x = ET.fromstring(response.content)
+        fault = x.find('.//{http://schemas.xmlsoap.org/soap/envelope/}Fault')
+        if fault is not None:
+            faultcode = fault.find('faultcode').text
+            faultstring = fault.find('faultstring').text
+            raise RuntimeError(faultcode, faultstring)
+
+        assert x.tag == '{http://schemas.xmlsoap.org/soap/envelope/}Envelope'
+        r = x.find('.//{http://ctkipservice.rsasecurity.com}' + outer)
+        ad = r.find('{http://ctkipservice.rsasecurity.com}AuthData')
+        assert ad.text == self.auth == response.headers.get('Authorization')
+        pd = r.find('{http://ctkipservice.rsasecurity.com}ProvisioningData')
+        rr = r.find('{http://ctkipservice.rsasecurity.com}' + inner)
+
+        return ET.fromstring(d64s(pd)), ET.fromstring(d64s(rr))
+
+########################################
+
+pd='''<?xml version="1.0"?><ProvisioningData><Version>5.0.2.440</Version><Manufacturer>RSA Security Inc.</Manufacturer><FormFactor/></ProvisioningData>'''
+req1='''<ClientHello xmlns="http://www.rsasecurity.com/rsalabs/otps/schemas/2005/11/ct-kip#" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" Version="1.0"><SupportedKeyTypes xmlns=""><Algorithm xsi:type="xsd:anyURI">http://www.rsasecurity.com/rsalabs/otps/schemas/2005/09/otps-wst#SecurID-AES</Algorithm></SupportedKeyTypes><SupportedEncryptionAlgorithms xmlns=""><Algorithm xsi:type="xsd:anyURI">http://www.w3.org/2001/04/xmlenc#rsa-1_5</Algorithm></SupportedEncryptionAlgorithms><SupportedMACAlgorithms xmlns=""><Algorithm xsi:type="xsd:anyURI">http://www.rsasecurity.com/rsalabs/otps/schemas/2005/11/ct-kip#ct-kip-prf-aes</Algorithm></SupportedMACAlgorithms></ClientHello>'''
+req2_tmpl='''<?xml version="1.0" encoding="UTF-8"?><ClientNonce xmlns="http://www.rsasecurity.com/rsalabs/otps/schemas/2005/11/ct-kip#" Version="1.0" SessionID="{session_id}"><EncryptedNonce xmlns="">{encrypted_client_nonce}</EncryptedNonce><Extensions xmlns="" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><Extension xmlns="" xmlns:ct-kip="http://www.rsasecurity.com/rsalabs/otps/schemas/2005/12/ct-kip#" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><Data>{server_nonce}</Data></Extension></Extensions></ClientNonce>'''
+
+p = argparse.ArgumentParser()
+p.add_argument('-v', '--verbose', action='count')
+p.add_argument('url')
+p.add_argument('activation_code')
+args = p.parse_args()
+
+s = requests.session()
+s.headers['user-agent'] = 'HTTPPOST'
+soap = Soapifier(args.url, args.activation_code)
+
+# send initial request
+req1 = soap.make_ClientRequest('StartService', pd, req1)
+
+# get session ID, server key, and server nonce in response
+raw_res1 = s.send(s.prepare_request(req1))
+if args.verbose:
+    print(raw_res1.text)
+pd_res1, res1 = soap.parse_ServerResponse(raw_res1)
+if args.verbose:
+    print(res1)
+
+session_id = res1.getroot().attrib['SessionID']
+k = res1.find('.//{http://www.w3.org/2000/09/xmldsig#}RSAKeyValue')
+mod = number.bytes_to_long(d64sb(k.find(
+  '{http://www.w3.org/2000/09/xmldsig#}Modulus').text))
+exp = number.bytes_to_long(d64sb(k.find(
+  '{http://www.w3.org/2000/09/xmldsig#}Exponent').text))
+pubk = RSA.construct({'n': mod, 'e': exp})
+pl = res1.find('.//Payload')
+server_nonce = d64sb(pl.find('Nonce').text)
+
+print("Got server nonce and RSA pubkey:\n{}\n{}".format(
+    hexlify(server_nonce), pubk.exportKey('PEM').decode()))
+
+# generate and encrypt client nonce
+client_nonce = random.getrandbits(16*8)
+cipher = PKCS1_OAEP.new(pubk)
+encrypted_client_nonce = cipher.encrypt(client_nonce)
+print("Generated client nonce:\n\tplaintext: {}\n\tencrypted: {}".format(
+    hexlify(client_nonce), hexlify(encrypted_client_nonce)))
+
+# send second request
+req2_filled = req2_tmpl.format(
+  session_id=session_id, encrypted_client_nonce=encrypted_client_nonce,
+  server_nonce=server_nonce)
+req2 = soap.make_ClientRequest('ServerFinished', pd, req2_filled)
+pd_res2, res2 = soap.parse_ServerResponse(s.send(s.prepare_request(req2)))
+
+if args.verbose:
+    print(res2)
+
+# get stuff from response
+key_id = d64b(res2.find('TokenID').text)
+token_id = d64b(res2.find('KeyID').text)
+key_exp = res2.find('KeyExpiryDate').text
+mac = d64b(res2.find('Mac'))
+print("Got key ID, token ID, key expiration date, and MAC:"
+      "\nKeyID: {}\nTokenID: {}\nExpiration: {}\nMAC: {}".format(
+        key_id, token_id, key_exp, mac))
---
+++ fakeserver.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python3
+import ssl
+import random
+import zlib
+from flask import Flask, request, abort, Response
+from xml.etree import ET
+from Crypto.PublicKey import RSA
+from Crypto.Util import number
+from Crypto.Cipher import PKCS1_OAEP
+
+from common import hexlify, unhexlify, d64b, e64b, e64s, e64bs, d64s, d64sb
+
+#####
+# Yay, encryption
+#####
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('server.pem')
+
+pubk = RSA.importKey(open('rsapubkey.pem').read())
+privk = RSA.importKey(open('rsaprivkey.pem').read())
+
+# The XML blobs in the protocol appear to indicate
+# RSAES-PKCS1-v1_5 (RFC8017), but it's actually RSAES-OAEP (RFC8017)
+cipher = PKCS1_OAEP.new(privk)
+
+########################################
+
+app = Flask(__name__)
+app.config.update(
+    PORT=4443,
+    HOST='localhost',
+)
+
+ns = {
+    'soapenv': 'http://schemas.xmlsoap.org/soap/envelope/',
+    'ctkip': 'http://ctkipservice.rsasecurity.com',
+    'ns0': 'http://www.rsasecurity.com/rsalabs/otps/schemas/2005/11/ct-kip#',
+}
+
+########################################
+
+# Handle the gawdaful SOAPy layer on the outside
+
+@app.route('/', methods=('POST',))
+def unsoap():
+    auth = request.headers.get('Authorization')
+    assert request.content_type == 'application/vnd.otps.ct-kip'
+
+    try:
+        x = ET.fromstring(request.data.decode())
+        assert x.tag == '{http://schemas.xmlsoap.org/soap/envelope/}Envelope'
+
+        cr = x.find('.//ctkip:ClientRequest', ns)
+        ad = cr.find('ctkip:AuthData', ns)
+        assert ad.text == auth
+        pd = cr.find('ctkip:ProvisioningData', ns)
+        r = cr.find('ctkip:Request', ns)
+
+        pdx = ET.fromstring(d64b(pd.text))
+        rx = ET.fromstring(d64b(r.text))
+
+        print("""
+Client sent:
+  Authorization:
+  ====================
+  {}
+
+  ProvisioningData:
+  ====================
+  {}
+
+  Request:
+  ====================
+  {}""".format(auth, ET.tostring(pdx).decode(), ET.tostring(rx).decode()))
+
+        # respond to client
+        sess = rx.attrib.get('SessionID')
+        if rx.tag == '{http://www.rsasecurity.com/rsalabs/otps/schemas/2005/11/ct-kip#}ClientHello':
+            res_pd, res_r = handle_ClientHello(sess, pdx, rx)
+            compr = False
+        elif rx.tag == '{http://www.rsasecurity.com/rsalabs/otps/schemas/2005/11/ct-kip#}ClientNonce':
+            res_pd, res_r = handle_ClientNonce(sess, pdx, rx)
+            compr = True
+
+        r = Response(mimetype='text/xml', response='''<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <soapenv:Body>
+    <ServerResponse xmlns="http://ctkipservice.rsasecurity.com">
+      <AuthData>{auth}</AuthData>
+      <ProvisioningData>{pd}</ProvisioningData>
+      <Response>{res}</Response>
+    </ServerResponse>
+  </soapenv:Body>
+</soapenv:Envelope>'''.format(auth=auth, pd=e64s(res_pd), res=e64s(res_r))
+        )
+        r.headers['X-Powered-By'] = 'Servlet/3.0 JSP/2.2'
+        setattr(r, 'allow_compression', compr)
+
+        print("""
+Server will send:
+  ProvisioningData:
+  ====================
+  {}
+
+  Response:
+  ====================
+  {}
+
+  SOAPified:
+  ====================
+  {}""".format(res_pd, res_r, r.data.decode()))
+
+
+        return r
+
+    except Exception as e:
+        print(e)
+        abort(500)
+
+########################################
+
+def handle_ClientHello(sess, pdx, rx):
+    rb = e64bs(number.long_to_bytes(random.getrandbits(16*8))).rstrip()
+    if sess is None:
+        sess = hexlify(number.long_to_bytes(random.getrandbits(17*8))).decode() + '-' + e64bs(number.long_to_bytes(random.getrandbits(56*8))+b'\0').rstrip()
+
+    return ET.tostring(pdx).decode(), '''<?xml version="1.0" encoding="UTF-8"?>
+<ServerHello xmlns="http://www.rsasecurity.com/rsalabs/otps/schemas/2005/12/ct-kip#" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" SessionID="{sess}" Status="Continue" Version="1.0">
+  <KeyType xmlns="">http://www.rsasecurity.com/rsalabs/otps/schemas/2005/09/otps-wst#SecurID-AES</KeyType>
+  <EncryptionAlgorithm xmlns="">http://www.w3.org/2001/04/xmlenc#rsa-1_5</EncryptionAlgorithm>
+  <EncryptionKey xmlns="" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <ds:KeyValue xmlns="http://www.rsasecurity.com/rsalabs/otps/schemas/2005/12/ct-kip#" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      <ds:RSAKeyValue xmlns="http://www.rsasecurity.com/rsalabs/otps/schemas/2005/12/ct-kip#" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <ds:Modulus>{mod}</ds:Modulus>
+        <ds:Exponent>{exp}</ds:Exponent>
+      </ds:RSAKeyValue>
+    </ds:KeyValue>
+  </EncryptionKey>
+  <Payload xmlns="" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <Nonce xmlns="">{rb}</Nonce>
+  </Payload>
+  <Extensions xmlns="" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <Extension xmlns:ct-kip="http://www.rsasecurity.com/rsalabs/otps/schemas/2005/12/ct-kip#" xmlns="" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      <Data>{rb}</Data>
+    </Extension>
+  </Extensions>
+  <MacAlgorithm xmlns="">http://www.rsasecurity.com/rsalabs/otps/schemas/2005/11/ct-kip#ct-kip-prf-aes</MacAlgorithm>
+</ServerHello>'''.format(sess = sess, rb = rb, mod = e64bs(number.long_to_bytes(pubk.n)).rstrip(), exp = e64bs(number.long_to_bytes(pubk.e)).rstrip() )
+
+
+def handle_ClientNonce(sess, pdx, rx):
+    # Decrypt the ClientNonce (this will be the token secret)
+    ct = d64b(rx.find('.//EncryptedNonce', ns).text)
+    print("ENcrypted ClientNonce: {}".format(hexlify(ct)))
+    print("DEcrypted ClientNonce: {}".format(hexlify(cipher.decrypt(ct))))
+
+    tid = e64s('%012d' % random.randint(1, 999999999999)).rstrip()         # Random 12-digit decimal number, b64enc
+    exp = '2019-01-01T00:00:00+00:00'                                      # ISO9601 datetime
+    rmb = e64bs(number.long_to_bytes(random.getrandbits(16*8))).rstrip()   # random MAC bytes... urk
+
+    pdr = '''<?xml version="1.0"?>\n<ProvisioningData><PinType>0</PinType><AddPIN>1</AddPIN></ProvisioningData>'''
+    r='''<?xml version="1.0" encoding="UTF-8"?>
+<ServerFinished xmlns="http://www.rsasecurity.com/rsalabs/otps/schemas/2005/12/ct-kip#" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" SessionID="{sess}" Status="Success" Version="1.0">
+  <TokenID xmlns="">{tid}</TokenID>
+  <KeyID xmlns="">{tid}</KeyID>
+  <KeyExpiryDate xmlns="">{exp}</KeyExpiryDate>
+  <ServiceID xmlns="">RSA CT-KIP</ServiceID>
+  <UserID xmlns=""/>
+  <Extensions xmlns="" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <Extension xmlns="" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" Critical="true">
+      <OTPFormat>Decimal</OTPFormat>
+      <OTPLength>8</OTPLength>
+      <OTPMode xmlns="http://www.rsasecurity.com/rsalabs/otps/schemas/2005/12/ct-kip#" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <Time xmlns="http://www.rsasecurity.com/rsalabs/otps/schemas/2005/12/ct-kip#" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" TimeInterval="60"/>
+      </OTPMode>
+    </Extension>
+  </Extensions>
+  <Mac xmlns="" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" MacAlgorithm="http://www.rsasecurity.com/rsalabs/otps/schemas/2005/12/ct-kip#ct-kip-prf-aes">
+    {rmb}
+  </Mac>
+</ServerFinished>'''.format(tid=e64s(tid).rstrip(), exp=exp, sess=sess, rmb=rmb)
+
+    return pdr, r
+
+
+########################################
+
+class Deflate(object):
+    def __init__(self, app, compress_level=6, minimum_size=500):
+        self.app = app
+        self.compress_level = compress_level
+        self.minimum_size = minimum_size
+        self.app.after_request(self.after_request)
+
+    def after_request(self, response):
+        if response.status_code < 200 or \
+           response.status_code >= 300 or \
+           response.direct_passthrough or \
+           not getattr(response, 'allow_compression', True) or \
+           len(response.data) < self.minimum_size or \
+           'Content-Encoding' in response.headers:
+            return response
+
+        response.data = zlib.compress(response.data, self.compress_level)
+        response.headers['Content-Encoding'] = 'deflate'
+        response.headers['Content-Length'] = len(response.data)
+        return response
+
+
+Deflate(app)
+app.run(host=app.config['HOST'],
+        port=app.config['PORT'],
+        debug=True,
+        ssl_context=context)
