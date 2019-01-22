ac57bab9179cdb6c6e1745adfb824f9e4070efe6
Daniel Lenski <dlenski@gmail.com>
2019-01-22 13:49:55 -0800

    make ct_kip_prf_aes() return a binary digest, not a pre-hexlified digest

--- client.py
+++ client.py
@@ -127,9 +127,7 @@ print("Got key ID, token ID, key expiration date, and MAC:"
         key_id, token_id, key_exp))
 
 # verify MAC and token
-K_TOKEN_hex = ct_kip_prf_aes(client_nonce, number.long_to_bytes(pubk.n), b"Key generation", R_S)
-K_TOKEN = unhexlify(K_TOKEN_hex)
-MAC_hex = ct_kip_prf_aes(K_TOKEN, b"MAC 2 Computation", R_C)
-MAC = unhexlify(MAC_hex)
+K_TOKEN = ct_kip_prf_aes(client_nonce, number.long_to_bytes(pubk.n), b"Key generation", R_S)
+MAC = ct_kip_prf_aes(K_TOKEN, b"MAC 2 Computation", R_C)
 if MAC==mac:
-    print("MAC verified. Token seed is: {}".format(K_TOKEN_hex))
+    print("MAC verified. Token seed is: {}".format(hexlify(K_TOKEN)))
